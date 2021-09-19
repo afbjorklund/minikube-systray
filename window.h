4f90fb01b4d1ca06d332778f8aa40e78ea767ae9
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-09-19 18:05:52 +0200

    Removed unused code from example

--- window.h
+++ window.h
@@ -90,13 +90,9 @@ protected:
 private slots:
     void setIcon(int index);
     void iconActivated(QSystemTrayIcon::ActivationReason reason);
-    void showMessage();
-    void messageClicked();
 
 private:
     void createStatusGroupBox();
-    void createIconGroupBox();
-    void createMessageGroupBox();
     void createActions();
     void createTrayIcon();
 
@@ -111,22 +107,7 @@ private:
     QLabel *statusLabel;
     QPushButton *startButton;
     QPushButton *stopButton;
-    QGroupBox *iconGroupBox;
-    QLabel *iconLabel;
     QComboBox *iconComboBox;
-    QCheckBox *showIconCheckBox;
-
-    QGroupBox *messageGroupBox;
-    QLabel *typeLabel;
-    QLabel *durationLabel;
-    QLabel *durationWarningLabel;
-    QLabel *titleLabel;
-    QLabel *bodyLabel;
-    QComboBox *typeComboBox;
-    QSpinBox *durationSpinBox;
-    QLineEdit *titleEdit;
-    QTextEdit *bodyEdit;
-    QPushButton *showMessageButton;
 
     QAction *minimizeAction;
     QAction *maximizeAction;
